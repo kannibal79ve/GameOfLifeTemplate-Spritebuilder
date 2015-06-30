2797558d77c90d675815c6f71a0ae9d8f6a3d8f9
Anibal Fernandez <anibal79ve@gmail.com>
2015-06-30 18:20:00 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Source/Grid.h
@@ -0,0 +1,18 @@
+//
+//  Grid.h
+//  GameOfLife
+//
+//  Created by Anibal Fernandez on 6/26/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+
+@property (nonatomic, assign) int totalAlive;
+@property (nonatomic, assign) int generation;
+
+
+
+@end
